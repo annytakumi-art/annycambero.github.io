ef906208afcad97c610fe532f2b215e04c01dbe2
Anny <annytakumi@gmail.com>
2026-06-20 17:50:39 +0200

    Añadiendo nuevo fondo

---
+++ aboutme.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', ( )  => {
+const ACERCADEMI =  document.getElementById ('aboutme');
+ if (!aboutme) {
+        console.error("Error en encontrar al contenedor de la web");
+        return;
+    }
+
+})
