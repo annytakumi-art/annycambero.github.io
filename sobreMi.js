f74c366a03581950ab9eda9ad944727ab237df4c
Anny <annytakumi@gmail.com>
2026-06-21 14:45:28 +0200

    haciendo sobre animado

--- sobreMi.js
+++ sobreMi.js
@@ -5,6 +5,7 @@ if (!sobreMi) {
         console.error("Error en encontrar al contenedor de la web");
         return;
     }
+    //esto es para que se quede guardada la opción de claro u oscuro
 const modoGuardado = localStorage.getItem('modoElegido');
 
 if (modoGuardado === 'oscuro') {
@@ -41,6 +42,52 @@ botonModoOscuro.addEventListener('click', () => {
         localStorage.setItem('modoElegido', 'oscuro');
     }else{
         localStorage.setItem('modoElegido', 'claro');
+    }
+});
+
+//haciendo el icono de contáctame animado
+
+const crearSobreAnimado = () => {
+    const contenedorMail = document.createElement('div');
+    contenedorMail.classList.add('iconoMail');
+
+    const regresaMail = document.createElement('div');
+    regresaMail.classList.add('regresaMail');
+
+    const cartaMail = document.createElement('div');
+    cartaMail.classList.add('cartaMail');
+
+    const frenteCarta = document.createElement('div');
+    frenteCarta.classList.add('frenteCarta');
+
+    const cartaSolapa = document.createElement('div');
+    cartaSolapa.classList.add('cartaSolapa');
+
+    contenedorMail.appendChild(regresaMail);
+    contenedorMail.appendChild(cartaMail);
+    contenedorMail.appendChild(frenteCarta);
+    contenedorMail.appendChild(cartaSolapa);
+
+    return contenedorMail;
+};
+botonContactame.addEventListener('click', () => {
+    vistaPrincipal.innerHTML = `
+        <section id="seccionContactame">
+            <h2> Contáctame </h2>
+            <p>¡Hablemos sobre código y diseño! Haz clic en el sobre para enviarme un correo.</p>
+            <div id="contenedor-sobre"></div> 
+        </section>
+    `;
+    
+
+    const espacioParaSobre = document.getElementById('contenedor-sobre');
+    
+    if (espacioParaSobre) {
+        const miSobre = crearSobreAnimado(); 
+        espacioParaSobre.appendChild(miSobre);
+        console.log("Sobre añadido exitosamente");
+    } else {
+        console.error("No se encontró el contenedor-sobre en el DOM");
     }
 });
 
@@ -84,6 +131,7 @@ const vistaPrincipal = document.createElement('main');
     const vistaProyectosHTML = `
         <h2> Mis proyectos </h2>
         
+        
     `;
     const vistaHabilidadesHTML =`
         <h2> Mis habilidades </h2>
@@ -103,8 +151,6 @@ const vistaPrincipal = document.createElement('main');
     botonSobreMi.addEventListener('click', () => cambiarVista(vistaSobreMiHTML));
     botonProyectos.addEventListener('click', () => cambiarVista(vistaProyectosHTML));
     botonHabilidades.addEventListener('click', () => cambiarVista(vistaHabilidadesHTML));
-    botonContactame.addEventListener('click', () => cambiarVista(vistaContactameHTML));
-
         cambiarVista(vistaSobreMiHTML);
 };
 configurarNavegacion();
